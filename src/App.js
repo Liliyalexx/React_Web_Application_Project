2f2dd11aa59b548ca01208f2e3acf7fd9d670a95
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-07-20 16:48:08 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+
+  render (){
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+return (
+  <div className='App'>
+  {loading ? (
+    <h4>loading...</h4>
+  ) : (
+    <h1>Hello {showName && name}</h1>
+  )}
+  </div>
+  
+);
+}
 }
 
 export default App;
