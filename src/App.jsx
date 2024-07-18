e30f8af2159b5e4ff1d97bd4f5c20503c340419a
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-07-18 14:52:40 -0700

    alert-please enter something created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
@@ -14,16 +15,16 @@ class App extends Component {
     loading: false,
     alert: null
   };
-  //async componentDidMount() {
+  async componentDidMount() {
     
-  //   this.setState({ loading: true });
+    this.setState({ loading: true });
 
-  //   const res = await axios.get(`https://api.github.com/users?client_id=$
-  //   {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-  //   {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/users?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-  //   this.setState({ users: res.data, loading: false });
-  // }
+    this.setState({ users: res.data, loading: false });
+  }
 
   // Search Githun users
   searchUsers = async text => {
@@ -41,28 +42,40 @@ class App extends Component {
   // Set Alert
   setAlert = (msg, type)=>{
     this.setState({alert:{msg, type}});
+    setTimeout(() => this.setState({alert: null}), 1000)//timer for alert message
     };
   
 
   render() {
     const {users, loading} = this.state;
     return (
-      <div className='App'>
-        <Navbar title="Github Finder" icon="fab fa-github" />
-        <div className='container'>
-        <Alert alert={this.state.alert}/>
-        <Search 
-        searchUsers={this.searchUsers} 
-        clearUsers={this.clearUsers} 
-        showClear={users.length > 0 ? true: false }
-        setAlert={this.setAlert}
-        />
-        <Users loading={loading} users={users} 
-        />
+      <Router>
+        <div className="App">
+          <Navbar title="Github Finder" icon="fab fa-github" />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                }
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
+
 
 export default App;
